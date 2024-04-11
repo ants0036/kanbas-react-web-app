e4dad907525eeae1fc9b140682695ea12f431772
ants0036 <anthony.s0036@gmail.com>
2024-04-11 17:57:09 -0400

    working

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,10 +1,22 @@
-import Nav from "../Nav";
+import { Routes, Route } from "react-router";
+import Courses from "./Courses";
+import Dashboard from "./Dashboard";
+import KanbasNavigator from "./Navigation";
+
 function Kanbas() {
-    return(
-       <div>
-         <Nav/>
-          <h1>Kanbas</h1>
-       </div>
-    );
- }
- export default Kanbas
+  return (
+    <div className="d-flex">
+      <KanbasNavigator />
+      <div>
+        <Routes>
+          <Route path="/Account" element={<h1>Account</h1>} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/Courses/:courseId" element={<Courses />} />
+          <Route path="/Calendar" element={<h1>Calendar</h1>} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default Kanbas;
